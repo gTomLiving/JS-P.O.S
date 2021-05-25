c06fbe8824eecd3f8bf775d83f2f088d33b99e67
Derek Jackson <d4jackson84@gmail.com>
2021-05-25 18:29:17 -0400

    receipt creation & styling

--- script.js
+++ script.js
@@ -44,9 +44,14 @@ let checkOutChangeDisplay = document.querySelector(".change");
 let checkOutBtn = document.querySelector('.final-check-out-button');
 
 //receipt 
+let receipt = document.querySelector('.receipt-container');
 let receiptName = document.querySelector('.receipt-item-name');
 let receiptQuantity = document.querySelector('.receipt-item-quantity');
-let receipt = document.querySelector('.receipt-container');
+let receiptTotalContainer = document.querySelector(".receipt-total");
+let receiptSubtotal = document.querySelector(".receipt-subtotal");
+let receiptTax = document.querySelector(".receipt-tax");
+let receiptTotal = document.querySelector(".receipt-total");
+let receiptCashOrCard = document.querySelector(".on-cash-or-card");
 
 
 
@@ -86,52 +91,57 @@ checkoutMenu.addEventListener("click", (event) => {
         let newTotal = 0;
         for (item of cart){
             newTotal += (((parseInt(item.price) * item.quantity) / 100) * 1.06);
-            console.log(newTotal);
             let cashGiven = checkOutCashInput.value;
-            console.log(checkOutCashInput.value);
             let changeDisplay = cashGiven - newTotal;
-            console.log(changeDisplay);
             checkOutChangeDisplay.innerText = `Change: $${changeDisplay.toFixed(2)}`;
 
         };
     };
+
+    // back to store 
     if (event.target.classList.contains("back-to-cart")){
-        console.log("yo");
         checkoutMenu.classList.toggle("check-out-menu");
         shoppingCart.classList.toggle("shopping-cart");
     };
+
+    // finishes checkout process & generates receipt
     if(event.target.classList.contains('final-check-out-button')) {
         event.preventDefault();
+        // generates receipt overlay
         receipt.classList.add('receipt');
+        
+        // begins totals compilation for receipt
         let formData = new FormData();
-
         formData.append('Grand Total', `${total.innerText}`);
         formData.append('tax', `${tax.innerText}`);
-        formData.append('total', `${newTotal}`);
-        
-        
+        formData.append('total', `${newTotal.toFixed(2)}`);
 
+        // fills in totals on receipt
+        receiptSubtotal.innerText = `Subtotal: ${formData.get('total')}`;
+        receiptTax.innerText = `${formData.get('tax')}`;
+        receiptTotal.innerText = `${formData.get('Grand Total')}`;
+        // for cash or card display switch
+        let cashGiven = checkOutCashInput.value;
+        let receiptChange = cashGiven - (newTotal * 1.06);
+        // cash or card functions
+        let cashOption = () => receiptCashOrCard.innerText = `Change: ${receiptChange.toFixed(2)}`;
+        let cardOption = () => receiptCashOrCard.innerText = `These numbers are accepted.`
+        // ternary to change bottom receipt display
+        cashGiven == 0 ? cardOption() : cashOption();
+       
+        // fills in cart items to receipt overlay
         for(item of cart) {
             let itemReceipt = document.createElement('p');
             let itemQuantity = document.createElement('p');
             itemReceipt.innerText = `${item.name}`;
             receiptName.appendChild(itemReceipt);
-
-
             itemQuantity.innerText = `${item.quantity}`;
             receiptQuantity.appendChild(itemQuantity);
-        }
-        
-        console.log(formData.get('total'));
-        console.log(formData.get('tax'));
-        console.log(formData.get('Grand Total'));
-
-        console.log(checkOutCashInput.value);
-
+        };
     }
 });
 
-// add to cart event
+// ADD TO CART EVENT
 right.addEventListener("click", (event) => {
     if (event.target.classList.contains("add-to-cart-button")){
         let addPrice = parseInt(event.target.getAttribute("data-price"));
@@ -151,68 +161,67 @@ right.addEventListener("click", (event) => {
         // create quantity box variables && appends for shopping cart display
          let createBox = document.createElement("div");
          createBox.setAttribute("class", "cart-change-quantity");
-
         // add left arrow to box
         let leftArrow = document.createElement("i");
         leftArrow.setAttribute("class", "ib ib-mdi-arrow-left-circle subtract");
         createBox.appendChild(leftArrow);
-
         // add quantity box to box
         let cartProductQuantityCreator = document.createElement("span");
         cartProductQuantityCreator.setAttribute("class", "cart-product-quantity");
         cartProductQuantityCreator.innerText = newCartObject.quantity;
         createBox.appendChild(cartProductQuantityCreator);
-
         // add right arrow to box
         let rightArrow = document.createElement("i");
         rightArrow.setAttribute("class", "ib ib-mdi-arrow-right-circle add");
         createBox.appendChild(rightArrow);    
 
-            // updates cart values for repeat items without creating new line
-            for (item of cart){
-                if (addName === item.name){
-                    alreadyInCart = true
-                    item.quantity++;
-                    let newItemPrice = parseInt(item.quantity) * parseInt(addPrice / 100).toFixed(2);
-                    cartProductQuantityCreator.innerText = item.quantity;
+        // updates cart values for repeat items without creating new line
+        for (item of cart){
+            if (addName === item.name){
+                alreadyInCart = true
+                item.quantity++;
+                let newItemPrice = parseInt(item.quantity) * parseInt(addPrice).toFixed(2) / 100;
+                cartProductQuantityCreator.innerText = item.quantity;
 
-                    // ****ASK KYLE WHY CART QUANTITY BOX WON"T UPDATE****
-                    console.log(cartProductQuantity);
-                    // cartProductQuantity.innerText = item.quantity;
-                    console.log(cartProductQuantityCreator.innerText);
-
-                    let boxQuantity = event.target.querySelector(".cart-product-quantity");
-                    console.log(boxQuantity);
-                    // boxQuantity.innerText = item.quantity;
-                        
+                // ****CART QUANTITY BOX WON"T UPDATE BUT aspects are working in background****
+                // create NodeList to correlate and match with item.quantity? --- see paper note
+                console.log(cartProductQuantityCreator);
+                
+                console.log(cartProductQuantityCreator.innerText);
+                // cartProductQuantity.innerText = item.quantity;
+                let boxQuantity = event.target.querySelector(".cart-product-quantity");
+                // console.log(boxQuantity.innerText);
+                // boxQuantity.innerText = item.quantity;
                     
-                    cartPriceDisplay.innerText = `$${newItemPrice.toFixed(2)}`;
-                    // updating total on checkout display
-                    checkOutTotalDisplay.innerText = cartPriceDisplay.innerText;
-                    break;
-                };        
-            };       
-    
-            if (alreadyInCart == false){
-                // insert quantity boxes into shopping cart
-                cartQuantityContainer.appendChild(createBox);      
-                // cartProductQuantity = document.querySelector(".cart-product-quantity");
-                newCartObject.quantity = 1;
-                // cartProductQuantity.innerText = newCartObject.quantity;
-                // console.log(cartProductQuantity.innerText);
-                cart.push(newCartObject);
-                // generate price box in shopping cart overlay
-                cartPriceDisplay = document.createElement("p");
-                cartPriceDisplay.innerText = `$${(addPrice / 100).toFixed(2)}`;
-                cartProductPriceHolder.appendChild(cartPriceDisplay);
+                
+                cartPriceDisplay.innerText = `$${newItemPrice.toFixed(2)}`;
                 // updating total on checkout display
                 checkOutTotalDisplay.innerText = cartPriceDisplay.innerText;
-                // generate name box
-                let cartItemNameDisplay = document.createElement("p");
-                cartItemNameDisplay.setAttribute("class", "name-in-cart")
-                cartItemNameDisplay.innerText = addName;
-                cartNameHolder.appendChild(cartItemNameDisplay);
-            };    
+                break;
+            };        
+        }; 
+
+        // updates cart values for new objects and creates new shopping cart overlay displays
+        if (alreadyInCart == false){
+            // insert quantity boxes into shopping cart
+            cartQuantityContainer.appendChild(createBox);      
+            // cartProductQuantity = document.querySelector(".cart-product-quantity");
+            newCartObject.quantity = 1;
+            // cartProductQuantity.innerText = newCartObject.quantity;
+            // console.log(cartProductQuantity.innerText);
+            cart.push(newCartObject);
+            // generate price box in shopping cart overlay
+            cartPriceDisplay = document.createElement("p");
+            cartPriceDisplay.innerText = `$${(addPrice / 100).toFixed(2)}`;
+            cartProductPriceHolder.appendChild(cartPriceDisplay);
+            // updating total on checkout display
+            checkOutTotalDisplay.innerText = cartPriceDisplay.innerText;
+            // generate name box
+            let cartItemNameDisplay = document.createElement("p");
+            cartItemNameDisplay.setAttribute("class", "name-in-cart")
+            cartItemNameDisplay.innerText = addName;
+            cartNameHolder.appendChild(cartItemNameDisplay);
+        };      
        
         console.log(cart);
     
@@ -224,7 +233,7 @@ right.addEventListener("click", (event) => {
         // All totals display
         subTotal.innerText = `Subtotal: $${newTotal.toFixed(2)}`;
         tax.innerText = `Tax: $${taxTotal.toFixed(2)}`;
-        total.innerText = `Total $${grandTotal.toFixed(2)}`;
+        total.innerText = `Total: $${grandTotal.toFixed(2)}`;
         cartSubTotal.innerText = `Subtotal: $${newTotal.toFixed(2)}`;
         cartTax.innerText = `Tax: $${taxTotal.toFixed(2)}`;
         cartTotal.innerText = `Total: $${grandTotal.toFixed(2)}`;
@@ -252,6 +261,14 @@ right.addEventListener("click", (event) => {
 
 
 });
+
+// closes receipt and resets page
+receipt.addEventListener("click", (event) => {
+    if (event.target.classList.contains("receipt-close")){
+        // event.target.classList.add("hidden");
+        location.reload();
+    }
+})
 
 
 // add to cart event
